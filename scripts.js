563fd130559595d20ce994f693c47d96459a74f2
unknown <sergey.bayraktar@ecdevstudio.com>
2022-09-24 12:22:33 +0300

    merge

--- scripts.js
+++
@@ -1,8 +0,0 @@
-let burger = document.getElementById('burger'),
-    mobileMenu = document.getElementById('mobile-menu');
-
-console.log(burger);
-
-burger.addEventListener('click', ()=> {
-  mobileMenu.style.display = 'flex';
-});
